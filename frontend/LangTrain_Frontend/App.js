756bffd03553432688ba7d43659837f4fffce644
Your Name <your.email@example.com>
2024-08-14 13:39:54 -0400

    added safe area view

--- frontend/LangTrain_Frontend/App.js
+++ frontend/LangTrain_Frontend/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import AppNavigator from "./Nav/AppNavigator";
 import "./firebase"; // Ensure Firebase is initialized
 import { SessionProvider } from "./hooks/AuthProvider.tsx";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <SessionProvider>
-      <AppNavigator />
-    </SessionProvider>
+    <SafeAreaView className="flex-1">
+      <SessionProvider>
+        <AppNavigator />
+      </SessionProvider>
+    </SafeAreaView>
   );
 }
